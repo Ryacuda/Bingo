9a4c04e50e8ae3023be1da00085b9382116fd08c
Ryacuda <adam.germain1@gmail.com>
2023-03-24 22:56:14 +0100

    Stitcher now knows what the created asset's size is, need to preprocess images before stitching

--- ImageStitcher.cpp
+++ ImageStitcher.cpp
@@ -46,7 +46,28 @@ void ImageStitcher::stitch()
 		}
 	}
 
+	if (img_paths.empty())
+	{
+		// no images found in the directory, so there's nothing to stitch
+		return;
+	}
+
 	// We create a support image with the right size
+	double r = ((double)m_cellwidth) / m_cellheight;
+	int l = ceil(sqrt(r * img_paths.size()));					// number of images along the y axis
+	int k = ceil( ((double)img_paths.size()) / l);				// number of images along the x axis
+
+	Magick::Image support_image(Magick::Geometry(k, l), Magick::Color(0,0,0,0));	// support image, transparent background
+
+	// loop through the images and overlay them onto the support, at their respective position
+	int i = 0;
+	while (i < img_paths.size())
+	{
+		// Open the image to overlay and resizes it to the desired dimensions
+		Magick::Image img(img_paths[i].string());
+
+		// support_image.composite(img)
+	}
 }
 
 // Other helpful functions
